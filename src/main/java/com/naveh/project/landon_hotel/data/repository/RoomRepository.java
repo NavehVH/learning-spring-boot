2cbd9103d3b312bf34ecd2354450e9b277d1eac4
NavehVH <naveh10@gmail.com>
2026-03-14 18:51:09 +0200

    Repository Room example use from SQL

---
+++ src/main/java/com/naveh/project/landon_hotel/data/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.naveh.project.landon_hotel.data.repository;
+
+import com.naveh.project.landon_hotel.data.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByRoomNumberIgnoreCase(String roomNumber);
+}
